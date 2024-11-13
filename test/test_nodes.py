f3050f5c6bef7a2ca4570f1084230bf7edc4cbc6
Xiaoyu <xiaoyumu@gmail.com>
2024-11-13 09:43:17 +0800

    Update test_nodes.py

--- test/test_nodes.py
+++ test/test_nodes.py
@@ -48,4 +48,29 @@ class PrimitiveBBOXTest(unittest.TestCase):
 
         expected = ([(356, 456, 512, 512)], 100, 200, 612, 712, 512, 512)
         self.assertEqual(expected, actual)
-        
+
+
+class StringToBBOXTest(unittest.TestCase):
+    def test_string_to_bbox_multiple_valid_boxes(self) -> None:
+        text = "(123, 456, 640, 480), (522, 356, 320, 240)"
+        box = nodes.StringToBBOX()
+        actual = box.string_to_bbox("XYWH", "XYWH", text)
+
+        expected = [(123, 456, 640, 480), (522, 356, 320, 240)]
+
+        self.assertEqual(expected, actual)
+
+    def test_string_to_bbox_multiple_valid_boxes_XYXY(self) -> None:
+        text = "(123, 456, 640, 480), (522, 356, 320, 240)"
+        box = nodes.StringToBBOX()
+        actual = box.string_to_bbox("XYWH", "XYXY", text)
+
+        expected = [(123, 456, 763, 936), (522, 356, 842, 596)]
+
+        self.assertEqual(expected, actual)
+
+    def test_string_to_bbox_with_invalid_input(self) -> None:
+        text = "(356, 320, 240)"
+        box = nodes.StringToBBOX()
+        with self.assertRaises(ValueError) as captured:
+            box.string_to_bbox("XYWH", "XYXY", text)
