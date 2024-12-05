de3033d0990869de74f46ac0d2d0e209e33ff84d
Xiaoyu <xiaoyumu@gmail.com>
2024-12-05 11:18:29 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,14 +1,16 @@
 from .box_nodes import PrimitiveBBOX, StringToBBOX
-from .image_nodes import AppyColor
+from .image_nodes import AppyColor, ColorAdjust
 
 NODE_CLASS_MAPPINGS = {
     "PrimitiveBBOX": PrimitiveBBOX,
     "StringToBBOX": StringToBBOX,
     "AppyColorToImage": AppyColor,
+    "AdjustImageColor": ColorAdjust,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PrimitiveBBOX": "Primitive BBOX",
     "StringToBBOX": "String To BBOX",
     "AppyColorToImage": "Apply Color To Image",
+    "AdjustImageColor": "Adjust Image Color with parameters"
 }
